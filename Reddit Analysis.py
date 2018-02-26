709a6173c004f16b3227fbe8a4489d4ffc34632b
Ailuropanda <rctan6@student.monash.edu>
2018-02-26 17:24:31 +1100

    Small Changes
    - Changing name of Analyze to Reddit Analysis
    - Making Reddit Analysis not read files

---
+++ Reddit Analysis.py
@@ -0,0 +1,43 @@
+import praw
+from textblob import TextBlob
+import math
+import time
+
+reddit = praw.Reddit(client_id = 'GEP04PzgZRRwvg',
+                     client_secret = 'N-jq0fnw4C9kCP-zOTQTfb_-0Bs',
+                     user_agent = 'subSentiment')
+
+
+def testModule(Subreddit_Name):
+    day_start = (time.time()- 10000)
+    day_end = time.time()
+
+    subreddit = reddit.subreddit(Subreddit_Name)
+
+    sub_submissions = subreddit.submissions(day_start, day_end)
+
+    sub_sentiment = 0
+    num_comments = 0
+
+    for submission in sub_submissions:
+        if not submission.stickied:
+            submission.comments.replace_more(limit=0)
+            for comment in submission.comments.list():
+                blob = TextBlob(comment.body)
+
+                comment_sentiment = blob.sentiment.polarity
+
+                sub_sentiment += comment_sentiment
+
+                num_comments += 1
+
+    print('/r/' + str(subreddit.display_name))
+    try:
+        print('Ratio:' + str(math.floor(sub_sentiment/num_comments*100)) + "\n")
+    except:
+        print("No comment sentiment." + "\n")
+
+
+Subreddit_Name = 'CryptoCurrency'
+
+testModule(Subreddit_Name)
