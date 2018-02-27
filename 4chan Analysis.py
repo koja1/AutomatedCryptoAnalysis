9db014a81d42e6e600286c9d1d2b1a7661ba0299
Ailuropanda <rctan6@student.monash.edu>
2018-02-27 13:25:06 +1100

    Update 4chan Analysis.py

--- 4chan Analysis.py
+++ 4chan Analysis.py
@@ -12,13 +12,16 @@ for thread in allthreads:
     blob = TextBlob(thread.topic.text_comment.lower())
 
     comment_sentiment = blob.sentiment.polarity  # Looks like this line does literally all the work, how does it work?
+    
+    #https://textblob.readthedocs.io/en/latest/quickstart.html#quickstart this is good explanation
 
     sentiment_value += comment_sentiment
 
     num_comments += 1
 
 print('/r/' + "BIZ")
-try:  # I wonder if try catch is better to use than "if num_comments > 0"
+try:  # I wonder if try catch is better to use than "if num_comments > 0" 
+    # we can time it some time
     print('Ratio:' + str(math.floor(sentiment_value/num_comments*100)) + "\n")
 except:
     print("No comment sentiment." + "\n")
